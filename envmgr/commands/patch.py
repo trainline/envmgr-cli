77705e110b566088e4143bd63ef04b2abb923cf6
Duncan Hall <himself@duncanhall.net>
2017-02-16 14:47:59 +0000

    Set amis and servers as instance members

--- envmgr/commands/patch.py
+++ envmgr/commands/patch.py
@@ -4,6 +4,8 @@ from envmgr.commands.base import BaseCommand
 from tabulate import tabulate
 
 class Patch(BaseCommand):
+    amis = []
+    servers = []
 
     def run(self):
         self.get_patch_status(**self.cli_args)
@@ -24,23 +26,19 @@ class Patch(BaseCommand):
                 3: '->',
                 4: p['to_ami']
                 }, result)
-
             messages.append(tabulate(table_data, tablefmt="plain"))
             self.show_result(result, messages)
 
 
     def get_patch_requirements(self, cluster, env, from_ami=None, to_ami=None):
-        servers = self.api.get_environment_servers(env)['Value']
-        all_amis = self.api.get_images()
-        
         # We're only interested in Windows as Linux instances auto-update
-        windows_amis = [ ami for ami in all_amis if ami['Platform'] == 'Windows' ]
-
-        # Validate AMI specs
-        self.validate_ami_compatibility(windows_amis, from_ami, to_ami)
-
+        self.amis = self.api.get_images()
+        self.amis = [ ami for ami in self.amis if ami['Platform'] == 'Windows' ]
+        self.validate_ami_compatibility(from_ami, to_ami)
+        
         # List of clusters' servers with AMI info
-        servers = [ server for server in servers if 
+        self.servers = self.api.get_environment_servers(env)['Value']
+        self.servers = [ server for server in self.servers if 
             'Ami' in server and server['Cluster'].lower() == cluster.lower() ]
         
         # Update any non-latest-stable if no "from ami" given
@@ -48,61 +46,58 @@ class Patch(BaseCommand):
             is_out_of_date = lambda ami,server: ami['Name'] == from_ami
         else:
             is_out_of_date = lambda ami,server: not ami['IsLatestStable']
-
+        
         # List of requirements to be considered for updates
-        matchers = [
+        # Prefer 'latest stable' info from image, not server
+        update_requirements = [
             lambda ami,server: ami['Name'] == server['Ami']['Name'],
             is_out_of_date
         ]
-        
-        # List of matching servers
-        servers_to_update = [ server for server in servers if 
-            any(ami for ami in windows_amis if 
-                all([ match(ami,server) for match in matchers ])
+        # List of servers with Windows AMI that matches update requirement
+        servers_to_update = [ server for server in self.servers if 
+            any(ami for ami in self.amis if 
+                all([ requirement(ami,server) for requirement in update_requirements ])
             )
         ]
+        # List of patches to apply
+        return map(self.create_patch_item, servers_to_update)
 
-        def patch_transform(s):
-            from_name = s['Ami']['Name']
-            target = self.get_target_ami(windows_amis, from_name)
-            to_name = target['Name']
-            return {
-                'server_name':s['Name'],
-                'from_ami':from_name,
-                'to_ami':to_name,
-                'server_role':s['Role'],
-                'services_count':len(s['Services']),
-                'instances_count':s['Size']['Current']
-            }
+    def create_patch_item(self, server):
+        from_name = server['Ami']['Name']
+        target = self.get_target_ami(from_name)
+        to_name = target['Name']
+        patch = {
+            'server_name': server['Name'],
+            'from_ami': from_name,
+            'to_ami': to_name,
+            'server_role': server['Role'],
+            'services_count': len(server['Services']),
+            'instances_count': server['Size']['Current']
+        }
+        return patch
 
-        patches = map(patch_transform, servers_to_update)
-        return patches
-
-    def get_target_ami(self, amis, from_name):
-        from_ami = self.get_ami_by_key(amis, 'Name', from_name)
+    def get_target_ami(self, from_name):
+        from_ami = self.get_ami_by_key('Name', from_name)
         ami_type = from_ami['AmiType']
-        
-        ami = [ ami for ami in amis if ami['AmiType'] == ami_type and ami['IsLatestStable'] == True]
+        ami = [ ami for ami in self.amis if ami['AmiType'] == ami_type and ami['IsLatestStable'] == True]
         return ami[0]
 
-    def get_ami_by_key(self, amis, key, value, unique=True):
-        ami = [ ami for ami in amis if ami[key] == value ]
+    def get_ami_by_key(self, key, value, unique=True):
+        ami = [ ami for ami in self.amis if ami[key] == value ]
         if not ami:
             raise ValueError('Could not find AMI with {0}={1}'.format(key, value))
         elif unique and len(ami) != 1:
             raise ValueError('Multiple AMI definitions found with {0}={1}'.format(key, value))
         return ami[0]
 
-    def validate_ami_compatibility(self, amis, from_name=None, to_name=None):
+    def validate_ami_compatibility(self, from_name=None, to_name=None):
         from_ami = None
         to_ami = None
 
         if from_name is not None:
-            from_ami = self.get_ami_by_key(amis, 'Name', from_name)
-
+            from_ami = self.get_ami_by_key('Name', from_name)
         if to_name is not None:
-            to_ami = self.get_ami_by_key(amis, 'Name', to_name)
-
+            to_ami = self.get_ami_by_key('Name', to_name)
         if from_ami is not None and to_ami is not None:
             if from_ami['AmiType'] != to_ami['AmiType']:
                 raise ValueError('AMI types for from_ami and to_ami must match')
